54b06951c53f296f7ef111198751726eb8b9cb6b
antonio-1337 <suteu.antonio@gmail.com>
2021-03-21 09:11:04 +0100

    Google Signin button + firebase setup

--- app/src/main/java/com/example/testapplication/ui/auth/LoginActivity.kt
+++ app/src/main/java/com/example/testapplication/ui/auth/LoginActivity.kt
@@ -1,16 +1,25 @@
 package com.example.testapplication.ui.auth
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.util.Log
 import android.view.View
 import android.widget.ProgressBar
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 import com.example.testapplication.R
 import com.example.testapplication.databinding.ActivityLoginBinding
+import com.example.testapplication.ui.home.HomeActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import utils.hide
 import utils.show
 import utils.toast
@@ -18,9 +27,16 @@ import java.util.*
 import kotlin.concurrent.schedule
 
 class LoginActivity : AppCompatActivity(), AuthListener {
+
+    private val RC_SIGN_IN: Int = 1
+    private var googleSignInClient: GoogleSignInClient? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_login)
+
+        //Google Auth Init
+        createRequest()
 
         val viewModel: AuthViewModel by viewModels()
         viewModel.authListener = this
@@ -29,6 +45,36 @@ class LoginActivity : AppCompatActivity(), AuthListener {
         bindingModel.viewModel = viewModel
 
     }
+
+    private fun createRequest() {
+        // Configure Google Sign In
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        // Build a GoogleSignInClient with the options specified by gso
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+    }
+
+    var resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            // There are no request codes
+            val data: Intent? = result.data
+            //doSomeOperations()
+        }
+    }
+
+    fun openSomeActivityForResult() {
+        val intent = Intent(this, HomeActivity::class.java)
+        resultLauncher.launch(intent)
+    }
+
+    /*private fun signIn() {
+        val signInIntent = googleSignInClient?.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }*/
+
 
     override fun onStarted() {
         //toast("Login Started")
