5df03d06c413d6ae9ef09637f109085e11670fd5
Elettra <e.messuri@campus.unimib.it>
2021-05-30 03:58:32 +0200

    taskname and taskcompleted activities added
    pusho prima di rompere tutto per sbaglio

---
+++ app/src/main/java/com/example/testapplication/pop_up_taskname.kt
@@ -0,0 +1,11 @@
+package com.example.testapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class pop_up_taskname : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pop_up_taskname)
+    }
+}
