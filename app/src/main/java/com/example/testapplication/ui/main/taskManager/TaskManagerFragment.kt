a6a692ae4bc88eb21f2d51dcaea81df8531c7103
tasca-a <81073987+tasca-a@users.noreply.github.com>
2021-06-10 16:18:06 +0200

    Day of the week automaticaly selected
    At first, the TaskManagerFragment selects the current day of the week, and then if the user change day the "selectedDay" value in the viewModel il changed accordingly

--- app/src/main/java/com/example/testapplication/ui/main/taskManager/TaskManagerFragment.kt
+++ app/src/main/java/com/example/testapplication/ui/main/taskManager/TaskManagerFragment.kt
@@ -2,11 +2,15 @@ package com.example.testapplication.ui.main.taskManager
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.NonNull
 import com.example.testapplication.R
+import com.example.testapplication.databinding.TaskManagerFragmentBinding
+import java.util.*
 
 class TaskManagerFragment : Fragment() {
 
@@ -17,9 +21,34 @@ class TaskManagerFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        //TODO: create everything lol
+        // Set the viewModel to the fragment
         viewModel = ViewModelProvider(this).get(TaskManagerViewModel::class.java)
 
-        return inflater.inflate(R.layout.task_manager_fragment, container, false)
+        // Setup binding object and inflate the fragment xml
+        val binding = TaskManagerFragmentBinding.inflate(inflater, container, false)
+
+        // Bind the viewModel in the layout to the viewModel class
+        binding.viewModel = viewModel
+
+        // This makes LiveData update the UI correctly
+        binding.lifecycleOwner = this
+
+        // Set the radio button of the current day checked
+        setRadioButtonToCurrentDay(binding)
+
+        return binding.root
     }
+
+    private fun setRadioButtonToCurrentDay(binding: TaskManagerFragmentBinding) {
+        when (viewModel.currentDayOfTheWeek) {
+            Calendar.MONDAY -> binding.radioButtonMonday.isChecked = true
+            Calendar.TUESDAY -> binding.radioButtonTuesday.isChecked = true
+            Calendar.WEDNESDAY -> binding.radioButtonWednesday.isChecked = true
+            Calendar.THURSDAY -> binding.radioButtonThursday.isChecked = true
+            Calendar.FRIDAY -> binding.radioButtonFriday.isChecked = true
+            Calendar.SATURDAY -> binding.radioButtonSaturday.isChecked = true
+            Calendar.SUNDAY -> binding.radioButtonSunday.isChecked = true
+        }
+    }
+
 }
--- app/src/main/java/com/example/testapplication/ui/main/taskManager/TaskManagerViewModel.kt
+++ app/src/main/java/com/example/testapplication/ui/main/taskManager/TaskManagerViewModel.kt
@@ -1,7 +1,17 @@
 package com.example.testapplication.ui.main.taskManager
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import java.util.*
 
 class TaskManagerViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    val calendar = Calendar.getInstance()
+    val currentDayOfTheWeek = calendar.get(Calendar.DAY_OF_WEEK)
+    var selectedDay: Int = currentDayOfTheWeek
+        set(value) {
+            Log.i("TaskManagerViewModel", "Giorno settato: $value")
+            field = value
+        }
+
 }
