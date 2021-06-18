c8a556578cd54f972e72269e5807ad53d2180898
tasca-a <81073987+tasca-a@users.noreply.github.com>
2021-06-18 16:29:14 +0200

    Added customizable basic timer
    Now you can set your basic timer and make it start properly.
    It was difficult.

--- app/src/main/java/com/example/testapplication/ui/main/timer/TimerFragment.kt
+++ app/src/main/java/com/example/testapplication/ui/main/timer/TimerFragment.kt
@@ -1,18 +1,26 @@
 package com.example.testapplication.ui.main.timer
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.testapplication.R
-import com.example.testapplication.databinding.TaskManagerFragmentBinding
+import android.widget.NumberPicker
+import android.widget.NumberPicker.OnValueChangeListener
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.example.testapplication.databinding.TimerFragmentBinding
+import com.example.testapplication.ui.main.timer.timerModes.TimerInterface
 
 class TimerFragment : Fragment() {
 
     private lateinit var viewModel: TimerViewModel
+
+    private lateinit var hoursPicker: NumberPicker
+    private lateinit var minutesPicker: NumberPicker
+    private lateinit var secondsPicker: NumberPicker
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -31,9 +39,48 @@ class TimerFragment : Fragment() {
         // This makes LiveData update the UI correctly
         binding.lifecycleOwner = this
 
-        // Initialize the viewModel for the timer to set up correctly
-        //viewModel.initialize(10000L)
+        // Setup the NumberPickers to display
+        numberPickerSetUp(binding)
+
+        // Observe the button status from the viewModel to update the UI
+        viewModel.buttonStatus.observe(viewLifecycleOwner, Observer { timerState ->
+            when (timerState) {
+                TimerInterface.Companion.TimerState.RUNNING_FOCUS -> {
+                    binding.numPicker.visibility = View.GONE
+                    binding.textViewTimer.visibility = View.VISIBLE
+                }
+            }
+        })
 
         return binding.root
+    }
+
+    fun numberPickerSetUp(binding: TimerFragmentBinding) {
+        hoursPicker = binding.numPickerHours
+        minutesPicker = binding.numPickerMinutes
+        secondsPicker = binding.numPickerSeconds
+
+        // Set boundaries for the various fields
+        hoursPicker.maxValue = 24
+        hoursPicker.minValue = 0
+
+        minutesPicker.maxValue = 59
+        minutesPicker.minValue = 0
+
+        secondsPicker.maxValue = 59
+        secondsPicker.minValue = 0
+
+        // Set the pickers listeners
+        hoursPicker.setOnValueChangedListener(OnValueChangeListener { picker, oldVal, newVal ->
+            viewModel.hours = newVal
+        })
+
+        minutesPicker.setOnValueChangedListener(OnValueChangeListener { picker, oldVal, newVal ->
+            viewModel.minutes = newVal
+        })
+
+        secondsPicker.setOnValueChangedListener(OnValueChangeListener { picker, oldVal, newVal ->
+            viewModel.seconds = newVal
+        })
     }
 }
--- app/src/main/java/com/example/testapplication/ui/main/timer/TimerViewModel.kt
+++ app/src/main/java/com/example/testapplication/ui/main/timer/TimerViewModel.kt
@@ -1,33 +1,63 @@
 package com.example.testapplication.ui.main.timer
 
-import androidx.databinding.BindingAdapter
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Transformations
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
 import com.example.testapplication.ui.main.timer.timerModes.Basic
 import com.example.testapplication.ui.main.timer.timerModes.TimerInterface
 import java.util.concurrent.TimeUnit
+
 
 class TimerViewModel : ViewModel() {
 
     // ATTENTION: The logic behind the pause and resume cycle are to be implemented here in the viewModel
     // to keep the various timer classes simple
-    private var timer: TimerInterface = Basic(10000L)
-    val timeLeft: LiveData<String> = Transformations.map(timer.timeLeft) { tLeft ->
-        var out = "ER:RO"
-        if (tLeft != null) {
-            out = String.format(
-                "%02d:%02d",
-                TimeUnit.MILLISECONDS.toMinutes(tLeft) % 60,
-                TimeUnit.MILLISECONDS.toSeconds(tLeft) % 60
-            )
+    var hours: Int = 0
+    var minutes: Int = 0
+    var seconds: Int = 0
+
+    // Control the button status based on the timer status
+    private var _buttonStatus = MutableLiveData(TimerInterface.Companion.TimerState.STOPPED)
+    val buttonStatus: LiveData<TimerInterface.Companion.TimerState>
+        get() = _buttonStatus
+
+    private lateinit var timer: TimerInterface
+
+    // The time left to be observed by the views
+    val timeLeft = MediatorLiveData<String>()
+
+    // Initialize a value for the timeLeft so that is not null when observed by the textView
+    init {
+        timeLeft.addSource(MutableLiveData<String>("00:00")){
+            timeLeft.value = it
         }
-        return@map out
     }
 
     fun start() {
+        // Create the correct timer with the correct time
+        var time: Long = 0L
+
+        time += TimeUnit.HOURS.toMillis(hours.toLong())
+        time += TimeUnit.MINUTES.toMillis(minutes.toLong())
+        time += TimeUnit.SECONDS.toMillis(seconds.toLong())
+
+        timer = Basic(time)
+
+        // Start the timer
         timer.start()
+
+        // Add to the timeLeft MediatorLiveData the new already initialized and started timer, so the view
+        // observing the timeLeft is correctly updated
+        timeLeft.addSource(timer.timeLeft){
+            val out = String.format(
+                    "%02d:%02d",
+                    TimeUnit.MILLISECONDS.toMinutes(it),
+                    TimeUnit.MILLISECONDS.toSeconds(it) % 60
+                )
+            timeLeft.value = out
+        }
+
+        // Change button status
+        _buttonStatus.value = TimerInterface.Companion.TimerState.RUNNING_FOCUS
+
     }
 
     fun stop() {
