a61cbb1159fc89f2bbf8e55e6e5b4c94641e968b
tasca-a <81073987+tasca-a@users.noreply.github.com>
2021-06-22 20:44:21 +0200

    Added dialog when an item is selected
    Now when the user clicks on a task, the proper dialogs pops up, and it can also navigate to te TimerFragment.

---
+++ app/src/main/java/com/example/testapplication/ui/main/taskManager/TaskSelectedDialogFragment.kt
@@ -0,0 +1,55 @@
+package com.example.testapplication.ui.main.taskManager
+
+import android.app.Dialog
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.DialogFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import com.example.testapplication.R
+import com.example.testapplication.databinding.DialogTaskBinding
+
+class TaskSelectedDialogFragment() : DialogFragment() {
+
+    // All the arguments from the navigation actions are stored here
+    val args: TaskSelectedDialogFragmentArgs by navArgs()
+
+    private lateinit var binding: DialogTaskBinding
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+
+        // Set up the binding object
+        binding = DataBindingUtil.inflate(LayoutInflater.from(context), R.layout.dialog_task, null, false)
+
+        // Setup the info to display in the dialog
+        binding.textViewName.text = args.taskName
+        binding.textViewDescription.text = args.taskDescription
+        binding.textViewTimerMode.text = args.timerMode
+
+        // Create the onClick listener for the "Start focusing!" button
+        binding.buttonStartFocusing.setOnClickListener(View.OnClickListener {
+            navigateToTimer()
+        })
+
+        return activity?.let {
+            // Use the Builder class for convenient dialog construction
+            val builder = AlertDialog.Builder(it)
+            builder.setView(binding.root)
+
+            // Create the AlertDialog object and return it
+            builder.create()
+        } ?: throw IllegalStateException("Activity cannot be null")
+    }
+
+    // Navigate to the timer fragment
+    // TODO: Passing arguments to the timer fragment
+    fun navigateToTimer(){
+        val action = TaskSelectedDialogFragmentDirections.actionTaskSelectedDialogFragmentToTimerFragment()
+        findNavController().navigate(action)
+    }
+}
